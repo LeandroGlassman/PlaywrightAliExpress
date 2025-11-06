44c5dece1798ac0e0cb591b172dfe7c09d231678
Leandro Glassman <glassmanleandro@gmail.com>
2025-11-05 22:57:59 -0300

    created products-page

---
+++ Pages/products-page.ts
@@ -0,0 +1,25 @@
+import { Page, Locator } from "@playwright/test";
+
+export class ProductsPage {
+  readonly page: Page;
+  readonly productsCards: Locator;
+  readonly secondPage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.productsCards = page.locator("#card-list > div");
+    this.secondPage = page.locator(".comet-pagination-item-2");
+  }
+
+  async navigateToSecondPage() {
+    await this.secondPage.click();
+  }
+
+  async getProductsCount() {
+    return await this.productsCards.count();
+  }
+
+  getProductCard(index: number) {
+    return this.productsCards.nth(index);
+  }
+}
